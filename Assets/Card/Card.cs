11b409b37ae3feb07fba6a4af96e3e509fd202dc
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-02-13 17:14:16 +0300

    Created a Card Prefab, and added a card back sprite

---
+++ Assets/Card/Card.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public Sprite backSprite;
+    public Sprite frontSprite;
+    public int value;
+    public SpriteRenderer cardSpriteRenderer;
+
+    public bool isFaceUp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cardSpriteRenderer.sprite = isFaceUp ? frontSprite : backSprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
