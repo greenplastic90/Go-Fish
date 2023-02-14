afef319641473ffff31f11d3d683415b065cc16e
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-02-14 15:15:50 +0300

    created a PlayerComponent prefab, and a GameSetuobject that instanciates PlayerComponents in certain possitioned depending on how many players there are

---
+++ Assets/Game Logic/GameLogic.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    public int numberOfPlayers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Game Setup/GameSetup.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetup : MonoBehaviour
+{
+    public int numberOfPlayers = 3;
+    public GameObject playerComponentPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        InstantiatePlayers(numberOfPlayers);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void InstantiatePlayer(int playerNumber, Vector3 position)
+    {
+        GameObject playerComponent = Instantiate(playerComponentPrefab, position, Quaternion.identity);
+        playerComponent.GetComponent<PlayerDetails>().playerNumber = playerNumber;
+    }
+
+    void InstantiatePlayers(int numberOfPlayers)
+    {
+        // Instantiate the player components based on the number of players
+        switch (numberOfPlayers)
+        {
+            case 2:
+                InstantiatePlayer(1, new Vector3(0, 0, 0)); // Bottom center
+                InstantiatePlayer(2, new Vector3(0, 8, 0));  // Top center
+                break;
+            case 3:
+                InstantiatePlayer(1, new Vector3(0, 0, 0));           // Bottom center
+                InstantiatePlayer(2, new Vector3(-4, 8, 0));          // Top left
+                InstantiatePlayer(3, new Vector3(4, 8, 0));           // Top right
+                break;
+            case 4:
+                InstantiatePlayer(1, new Vector3(0, 0, 0));           // Bottom center
+                InstantiatePlayer(2, new Vector3(0, 8, 0));           // Top center
+                InstantiatePlayer(3, new Vector3(-7, 4, 0));          // Left center
+                InstantiatePlayer(4, new Vector3(7, 4, 0));           // Right center
+                break;
+            case 5:
+                InstantiatePlayer(1, new Vector3(0, 0, 0));           // Bottom center
+                InstantiatePlayer(2, new Vector3(-4, 8, 0));          // Top left
+                InstantiatePlayer(3, new Vector3(4, 8, 0));           // Top right
+                InstantiatePlayer(4, new Vector3(-7, 4, 0));          // Left center
+                InstantiatePlayer(5, new Vector3(7, 4, 0));           // Right center
+                break;
+            default:
+                Debug.LogError("Unsupported number of players: " + numberOfPlayers);
+                break;
+        }
+    }
+}
