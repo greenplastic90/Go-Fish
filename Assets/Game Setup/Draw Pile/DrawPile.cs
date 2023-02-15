9b12f5dde0b7f407f19b429e6dfc9a6071c60947
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-02-15 12:50:15 +0300

    Moved all DrawPile Code into GameSetup, added all instances of Card created to drawPile List, added a DealCards function that runs when after all cards have been instanciated

--- Assets/Game Setup/Draw Pile/DrawPile.cs
+++ Assets/Game Setup/Draw Pile/DrawPile.cs
@@ -1,96 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class CardData
-{
-    public Sprite frontSprite;
-    public int value;
 
-}
 public class DrawPile : MonoBehaviour
 {
-    public GameObject cardPrefab;
-    public List<CardData> cardData;
-    public List<GameObject> drawPile;
-    public float timeBetweenCards;
+
+
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
 
-        CreateSuffledDrawPile();
-        Debug.Log("drawpile " + drawPile.Count);
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-    }
-    void CreateSuffledDrawPile()
-    {
-        //! Creating a List of CardData with all the sprites and their values
-        cardData = new List<CardData>();
-        drawPile = new List<GameObject>();
-
-        //? creating an array of all the sprites in our sprite sheet
-        Sprite[] sprites = Resources.LoadAll<Sprite>("Card_Sprites");
-
-        for (int i = 0; i < sprites.Length; i++)
-        {
-            CardData card = new CardData();
-            card.frontSprite = sprites[i];
-
-            //? sprites have names like "D2" and "S13" to mean Diamond 2 and Spade King respectivly, we want the value from the name.
-            string stringValue = sprites[i].name.Substring(1);
-            card.value = int.Parse(stringValue);
-
-            cardData.Add(card);
-        }
-
-        //! Shuffle the cardData list
-        Shuffle(cardData);
-
-        //! Instanciate Cards using the CardData List one by one with a delay in between each card
-        StartCoroutine(InstantiateCards());
-    }
-
-    IEnumerator InstantiateCards()
-    {
-        float z = 0;
-        foreach (CardData card in cardData)
-        {
-            //! Instanciate the card
-            GameObject newCard = Instantiate(cardPrefab, transform.position, transform.rotation);
-            newCard.GetComponent<Card>().frontSprite = card.frontSprite;
-            newCard.GetComponent<Card>().value = card.value;
-            //? give the card a random position within a certain range and a random z rotation
-            newCard.transform.localPosition = new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), z);
-            z -= 0.01f;
-            newCard.transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
-
-
-
-
-            //! Add a delay before instantiating the next card
-            yield return new WaitForSeconds(timeBetweenCards);
-        }
-    }
-
-    void Shuffle(List<CardData> list)
-    {
-        for (int i = 0; i < list.Count; i++)
-        {
-            CardData temp = list[i];
-            int randomIndex = Random.Range(0, list.Count);
-            list[i] = list[randomIndex];
-            list[randomIndex] = temp;
-        }
-    }
-
-    void DealCards()
-    {
 
     }
+
+
+
+
+
+
+
 }
 
--- Assets/Game Setup/GameSetup.cs
+++ Assets/Game Setup/GameSetup.cs
@@ -2,41 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class CardData
+{
+    public Sprite frontSprite;
+    public int value;
+
+}
+
 public class GameSetup : MonoBehaviour
 {
-    public int numberOfPlayers = 3;
+    private List<CardData> cardData;
+    public GameObject cardPrefab;
+
+    public List<GameObject> drawPile;
+    private int numberOfCardsEachPlayerDraws;
+    public float timeBetweenCards;
+    //! Number of players has to be between no less than 2 and no greater than 5
+    public int numberOfPlayers;
 
     public GameObject drawPilePrefab;
     public GameObject playerComponentPrefab;
 
-    public List<GameObject> PlayerComponents;
+    public List<GameObject> playerComponents;
     // Start is called before the first frame update
 
     void Start()
     {
-        PlayerComponents = new List<GameObject>();
+        playerComponents = new List<GameObject>();
+        cardData = new List<CardData>();
+        drawPile = new List<GameObject>();
+
+        numberOfCardsEachPlayerDraws = numberOfPlayers < 3 ? 5 : 7;
+
         InstantiatePlayers(numberOfPlayers);
-        InstantiateDrawPile();
-        Debug.Log("List " + PlayerComponents.Count);
-    }
+        CreateSuffledDrawPile();
 
-    // Update is called once per frame
-    void Update()
-    {
 
     }
 
-    void InstantiateDrawPile()
-    {
-        Instantiate(drawPilePrefab, new Vector3(0, 0, 0), Quaternion.identity);
-    }
-
-    void InstantiatePlayer(int playerNumber, Vector3 position)
-    {
-        GameObject playerComponent = Instantiate(playerComponentPrefab, position, Quaternion.identity);
-        playerComponent.GetComponent<PlayerDetails>().playerNumber = playerNumber;
-        PlayerComponents.Add(playerComponent);
-    }
 
     void InstantiatePlayers(int numberOfPlayers)
     {
@@ -70,4 +73,87 @@ public class GameSetup : MonoBehaviour
                 break;
         }
     }
+
+    void InstantiatePlayer(int playerNumber, Vector3 position)
+    {
+        GameObject playerComponent = Instantiate(playerComponentPrefab, position, Quaternion.identity);
+        playerComponent.name = "Player " + playerNumber; // uniqe object name
+        playerComponent.GetComponent<PlayerDetails>().playerNumber = playerNumber;
+        playerComponents.Add(playerComponent);
+    }
+
+
+    void DealCards()
+    {
+        Debug.Log("DealCards");
+
+    }
+    void CreateSuffledDrawPile()
+    {
+        //! Creating a List of CardData with all the sprites and their values
+
+
+        //? creating an array of all the sprites in our sprite sheet
+        Sprite[] sprites = Resources.LoadAll<Sprite>("Card_Sprites");
+
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            CardData card = new CardData();
+            card.frontSprite = sprites[i];
+
+            //? sprites have names like "D2" and "S13" to mean Diamond 2 and Spade King respectivly, we want the value from the name.
+            string stringValue = sprites[i].name.Substring(1);
+            card.value = int.Parse(stringValue);
+
+            cardData.Add(card);
+        }
+
+        //! Shuffle the cardData list
+        Shuffle(cardData);
+
+        //! Instanciate Cards using the CardData List one by one with a delay in between each card
+        StartCoroutine(InstantiateCards());
+
+
+    }
+
+    IEnumerator InstantiateCards()
+    {
+        float z = 0;
+        foreach (CardData card in cardData)
+        {
+            //! Instanciate the card
+            GameObject newCard = Instantiate(cardPrefab, transform.position, transform.rotation);
+            newCard.name = "Card " + card.frontSprite.name; // uniqe object name in Unity
+            newCard.GetComponent<Card>().frontSprite = card.frontSprite;
+            newCard.GetComponent<Card>().value = card.value;
+            //? give the card a random position within a certain range and a random z rotation
+            newCard.transform.localPosition = new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), z);
+            z -= 0.01f;
+            newCard.transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
+
+            drawPile.Add(newCard);
+
+
+
+            //! Add a delay before instantiating the next card
+            yield return new WaitForSeconds(timeBetweenCards);
+        }
+
+        DealCards();
+
+
+    }
+    void Shuffle(List<CardData> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            CardData temp = list[i];
+            int randomIndex = Random.Range(0, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
+    }
+
+
 }
