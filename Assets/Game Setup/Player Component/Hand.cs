4d64d988decec473a112603093ef753158e669ec
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-02-17 15:37:10 +0300

    MoveToDesiredPosition now moves cards smoothly

--- Assets/Game Setup/Player Component/Hand.cs
+++ Assets/Game Setup/Player Component/Hand.cs
@@ -9,7 +9,7 @@ public class Hand : MonoBehaviour
     private int lastCardCount;
 
     private float cardsOffset = 0.5f;
-    public float returnSpeed = 1f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -39,17 +39,21 @@ public class Hand : MonoBehaviour
 
     private void AdjustCardPositions()
     {
-
-
-
+        // Store all desired positions in a list
+        List<Vector3> desiredPositions = new List<Vector3>();
         for (int i = 0; i < cardsInHand.Count; i++)
         {
             Vector3 newPosition = generateCardPosition(i, cardsInHand.Count);
-            cardsInHand[i].transform.position = newPosition;
+            desiredPositions.Add(newPosition);
         }
 
-
+        // Start coroutine for each card
+        for (int i = 0; i < cardsInHand.Count; i++)
+        {
+            StartCoroutine(MoveToDesiredPosition(cardsInHand[i], desiredPositions[i]));
+        }
     }
+
     private Vector3 generateCardPosition(int i, int numberOfCards)
     {
 
@@ -61,13 +65,31 @@ public class Hand : MonoBehaviour
 
 
 
-    // public IEnumerator MoveToDesiredPostion(Vector3 desiredPosition)
-    // {
-    //     while (Vector3.Distance(transform.position, desiredPosition) > 0.1f)
-    //     {
-    //         transform.position = Vector3.Lerp(transform.position, desiredPosition, returnSpeed * Time.deltaTime);
-    //         yield return null;
-    //     }
-    //     transform.position = desiredPosition;
-    // }
+    public IEnumerator MoveToDesiredPosition(GameObject card, Vector3 desiredPosition)
+    {
+        float duration = 0.5f; // Duration of the movement
+        float time = 0f; // Time elapsed since the start of the movement
+
+        Vector3 startingPosition = card.transform.position; // Starting position of the card
+
+        // Loop until the movement is complete
+        while (time < duration)
+        {
+            // Calculate the new position of the card based on the time elapsed and the desired position
+            Vector3 newPosition = Vector3.Lerp(startingPosition, desiredPosition, time / duration);
+
+            // Set the position of the card to the new position
+            card.transform.position = newPosition;
+
+            // Increment the time elapsed
+            time += Time.deltaTime;
+
+            // Wait until the next frame
+            yield return null;
+        }
+
+        // Set the position of the card to the desired position (in case of rounding errors)
+        card.transform.position = desiredPosition;
+    }
+
 }
