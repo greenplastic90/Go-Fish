3b9e197e20ddb1c798b516a7846ee9281e188eff
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-02-18 14:43:42 +0300

    refactored code so that drawPile is its own prefab, its instanciated in GameSetup

---
+++ Assets/Draw Pile/DrawPile.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawPile : MonoBehaviour
+{
+    //? Public
+    public float timeBetweenInstanciatingCards;
+    public List<GameObject> drawPile;
+    public GameObject cardPrefab;
+    public List<GameObject> playerComponents;
+
+    public GameSetup gameSetup;
+
+    //? Private
+    private List<CardData> cardData;
+    private int numberOfCardsToDealAtGameStart;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        gameSetup = GameObject.Find("Game Setup").GetComponent<GameSetup>();
+        cardData = new List<CardData>();
+        drawPile = new List<GameObject>();
+        playerComponents = gameSetup.playerComponents;
+        numberOfCardsToDealAtGameStart = playerComponents.Count < 4 ? 7 : 5;
+        CreateSuffledDrawPile();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CreateSuffledDrawPile()
+    {
+        //! Creating a List of CardData with all the sprites and their values
+
+
+        //? creating an array of all the sprites in our sprite sheet
+        Sprite[] sprites = Resources.LoadAll<Sprite>("Card_Sprites");
+
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            CardData card = new CardData();
+            card.frontSprite = sprites[i];
+
+            //? sprites have names like "D2" and "S13" to mean Diamond 2 and Spade King respectivly, we want the value from the name.
+            string stringValue = sprites[i].name.Substring(1);
+            card.value = int.Parse(stringValue);
+
+            cardData.Add(card);
+        }
+
+        //! Shuffle the cardData list
+        Shuffle(cardData);
+
+        //! Instanciate Cards using the CardData List one by one with a delay in between each card
+        StartCoroutine(InstantiateCards());
+
+
+    }
+    void Shuffle(List<CardData> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            CardData temp = list[i];
+            int randomIndex = Random.Range(0, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
+    }
+
+    IEnumerator InstantiateCards()
+    {
+
+        float z = 0;
+        foreach (CardData card in cardData)
+        {
+            // Instanciate the card
+            GameObject newCard = Instantiate(cardPrefab, transform.position, transform.rotation);
+            newCard.transform.SetParent(gameObject.transform); // attach Card to the GameObject this script is on
+            newCard.name = "Card " + card.frontSprite.name; // uniqe object name in Unity
+            newCard.GetComponent<Card>().frontSprite = card.frontSprite;
+            newCard.GetComponent<Card>().value = card.value;
+            //? give the card a random position within a certain range and a random z rotation
+            newCard.transform.localPosition = new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), z);
+            z -= 0.01f;
+            newCard.transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
+
+            drawPile.Add(newCard);
+
+
+
+            // Add a delay before instantiating the next card
+            yield return new WaitForSeconds(timeBetweenInstanciatingCards);
+        }
+
+        DealCards();
+
+
+    }
+    void DealCards()
+    {
+        Debug.Log("Player Count => " + playerComponents.Count);
+
+        if (drawPile.Count < numberOfCardsToDealAtGameStart * playerComponents.Count)
+        {
+            Debug.LogError("Not enough cards in the draw pile to deal.");
+            return;
+        }
+
+        StartCoroutine(DealCardsCoroutine());
+    }
+    IEnumerator DealCardsCoroutine()
+    {
+        for (int i = 0; i < numberOfCardsToDealAtGameStart; i++)
+        {
+            foreach (GameObject player in playerComponents)
+            {
+                if (drawPile.Count > 0)
+                {
+                    int lastIndex = drawPile.Count - 1;
+                    int playerNumber = player.GetComponent<PlayerDetails>().playerNumber;
+                    GameObject card = drawPile[lastIndex];
+                    Transform hand = player.transform.Find("Hand");
+                    Vector3 startPos = card.transform.position;
+                    Vector3 endPos = hand.position;
+                    Quaternion startRot = card.transform.rotation;
+                    Quaternion endRot = Quaternion.Euler(hand.position);
+
+
+                    float t = 0;
+                    while (t < 1)
+                    {
+                        t += Time.deltaTime * 3;
+                        card.transform.position = Vector3.Lerp(startPos, endPos, t);
+                        card.transform.rotation = Quaternion.Lerp(startRot, endRot, t);
+                        yield return null;
+
+
+                    }
+
+                    //? Only flip card for player 1
+                    if (playerNumber == 1) { card.GetComponent<Card>().ToggleFaceUp(true); }
+                    card.transform.SetParent(hand, true);
+                    hand.GetComponent<Hand>().AddCard(card);
+                    drawPile.RemoveAt(lastIndex);
+                    yield return new WaitForSeconds(timeBetweenInstanciatingCards);
+                }
+            }
+        }
+    }
+}
--- Assets/Game Setup/GameSetup.cs
+++ Assets/Game Setup/GameSetup.cs
@@ -11,64 +11,48 @@ public class CardData
 
 public class GameSetup : MonoBehaviour
 {
-    public GameObject drawPileObject;
-    private List<CardData> cardData;
-    public GameObject cardPrefab;
-
-    public List<GameObject> drawPile;
-    private int numberOfCardsEachPlayerDraws;
-    public float timeBetweenInstanciatingCards;
-
     //! Number of players has to be between no less than 2 and no greater than 5
     public int numberOfPlayers;
-
     public GameObject drawPilePrefab;
     public GameObject playerComponentPrefab;
-
     public List<GameObject> playerComponents;
-    // Start is called before the first frame update
 
+    // Start is called before the first frame update
     void Start()
     {
         playerComponents = new List<GameObject>();
-        cardData = new List<CardData>();
-        drawPile = new List<GameObject>();
-
-        numberOfCardsEachPlayerDraws = numberOfPlayers < 3 ? 7 : 5;
-        // numberOfCardsEachPlayerDraws = 1;
-
         InstantiatePlayers(numberOfPlayers);
-        CreateSuffledDrawPile();
-
+        InstanciateDrawPile();
     }
 
-
-
-
-
-    //! Instanciating players
+    // Instanciating players
     void InstantiatePlayers(int numberOfPlayers)
     {
+        if (numberOfPlayers < 2 || numberOfPlayers > 5)
+        {
+            Debug.LogError("Number of players can not be less than 2 or more than 5");
+            return;
+        }
         // Instantiate the player components based on the number of players
         switch (numberOfPlayers)
         {
             case 2:
-                InstantiatePlayer(1, new Vector3(0, -4, 0)); // Bottom center
-                InstantiatePlayer(2, new Vector3(0, 4, 0));  // Top center
+                InstantiatePlayer(1, new Vector3(0, -4, 0));          // Bottom center
+                InstantiatePlayer(2, new Vector3(0, 4, 0));           // Top center
                 break;
             case 3:
-                InstantiatePlayer(1, new Vector3(0, -4, 0));           // Bottom center
+                InstantiatePlayer(1, new Vector3(0, -4, 0));          // Bottom center
                 InstantiatePlayer(2, new Vector3(-4, 4, 0));          // Top left
                 InstantiatePlayer(3, new Vector3(4, 4, 0));           // Top right
                 break;
             case 4:
-                InstantiatePlayer(1, new Vector3(0, -4, 0));           // Bottom center
+                InstantiatePlayer(1, new Vector3(0, -4, 0));          // Bottom center
                 InstantiatePlayer(2, new Vector3(-7, 0, 0));          // Left center
                 InstantiatePlayer(3, new Vector3(0, 4, 0));           // Top center
                 InstantiatePlayer(4, new Vector3(7, 0, 0));           // Right center
                 break;
             case 5:
-                InstantiatePlayer(1, new Vector3(0, -4, 0));           // Bottom center
+                InstantiatePlayer(1, new Vector3(0, -4, 0));          // Bottom center
                 InstantiatePlayer(2, new Vector3(-7, 0, 0));          // Left center
                 InstantiatePlayer(3, new Vector3(-4, 4, 0));          // Top left
                 InstantiatePlayer(4, new Vector3(4, 4, 0));           // Top right
@@ -88,128 +72,10 @@ public class GameSetup : MonoBehaviour
         playerComponents.Add(playerComponent);
     }
 
-
-
-    //! Darw pile 
-    void DealCards()
+    void InstanciateDrawPile()
     {
-        if (drawPile.Count < numberOfCardsEachPlayerDraws * playerComponents.Count)
-        {
-            Debug.LogError("Not enough cards in the draw pile to deal.");
-            return;
-        }
-
-        StartCoroutine(DealCardsCoroutine());
-    }
-
-    IEnumerator DealCardsCoroutine()
-    {
-        for (int i = 0; i < numberOfCardsEachPlayerDraws; i++)
-        {
-            foreach (GameObject player in playerComponents)
-            {
-                if (drawPile.Count > 0)
-                {
-                    int lastIndex = drawPile.Count - 1;
-                    int playerNumber = player.GetComponent<PlayerDetails>().playerNumber;
-                    GameObject card = drawPile[lastIndex];
-                    Transform hand = player.transform.Find("Hand");
-                    Vector3 startPos = card.transform.position;
-                    Vector3 endPos = hand.position;
-                    Quaternion startRot = card.transform.rotation;
-                    Quaternion endRot = Quaternion.Euler(hand.position);
-
-
-                    float t = 0;
-                    while (t < 1)
-                    {
-                        t += Time.deltaTime * 3;
-                        card.transform.position = Vector3.Lerp(startPos, endPos, t);
-                        card.transform.rotation = Quaternion.Lerp(startRot, endRot, t);
-                        yield return null;
-
-
-                    }
-
-                    //? Only flip card for player 1
-                    if (playerNumber == 1) { card.GetComponent<Card>().ToggleFaceUp(true); }
-                    card.transform.SetParent(hand, true);
-                    hand.GetComponent<Hand>().AddCard(card);
-                    drawPile.RemoveAt(lastIndex);
-                    yield return new WaitForSeconds(timeBetweenInstanciatingCards);
-                }
-            }
-        }
-    }
-
-    void CreateSuffledDrawPile()
-    {
-        //! Creating a List of CardData with all the sprites and their values
-
-
-        //? creating an array of all the sprites in our sprite sheet
-        Sprite[] sprites = Resources.LoadAll<Sprite>("Card_Sprites");
-
-        for (int i = 0; i < sprites.Length; i++)
-        {
-            CardData card = new CardData();
-            card.frontSprite = sprites[i];
-
-            //? sprites have names like "D2" and "S13" to mean Diamond 2 and Spade King respectivly, we want the value from the name.
-            string stringValue = sprites[i].name.Substring(1);
-            card.value = int.Parse(stringValue);
-
-            cardData.Add(card);
-        }
-
-        //! Shuffle the cardData list
-        Shuffle(cardData);
-
-        //! Instanciate Cards using the CardData List one by one with a delay in between each card
-        StartCoroutine(InstantiateCards());
-
+        GameObject drawPile = Instantiate(drawPilePrefab, Vector3.zero, Quaternion.identity);
+        drawPile.name = "Draw Pile";
 
     }
-
-    IEnumerator InstantiateCards()
-    {
-
-        float z = 0;
-        foreach (CardData card in cardData)
-        {
-            //! Instanciate the card
-            GameObject newCard = Instantiate(cardPrefab, transform.position, transform.rotation);
-            newCard.transform.SetParent(drawPileObject.transform, true);
-            newCard.name = "Card " + card.frontSprite.name; // uniqe object name in Unity
-            newCard.GetComponent<Card>().frontSprite = card.frontSprite;
-            newCard.GetComponent<Card>().value = card.value;
-            //? give the card a random position within a certain range and a random z rotation
-            newCard.transform.localPosition = new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), z);
-            z -= 0.01f;
-            newCard.transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
-
-            drawPile.Add(newCard);
-
-
-
-            //! Add a delay before instantiating the next card
-            yield return new WaitForSeconds(timeBetweenInstanciatingCards);
-        }
-
-        DealCards();
-
-
-    }
-    void Shuffle(List<CardData> list)
-    {
-        for (int i = 0; i < list.Count; i++)
-        {
-            CardData temp = list[i];
-            int randomIndex = Random.Range(0, list.Count);
-            list[i] = list[randomIndex];
-            list[randomIndex] = temp;
-        }
-    }
-
-
 }
